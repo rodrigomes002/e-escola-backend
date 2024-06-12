25ece9db2fb6872318fbd7d8a4b205e058259887
caleb <caleb@DESKTOP-K0V5AD6>
2024-06-11 23:53:35 -0300

    criando entidade e controller escola

---
+++ eEscola.API/Controllers/EscolasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eEscola.API.Controllers
+{
+    [Route("api/escolas")]
+    [ApiController]
+    public class EscolasController : ControllerBase
+    {
+        public EscolasController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
