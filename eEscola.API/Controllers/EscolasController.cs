92f282b0e0212f14075184e44ac8a4b1a1911687
caleb <caleb@DESKTOP-K0V5AD6>
2024-06-13 23:21:07 -0300

    Criado CRUD de Escola

--- eEscola.API/Controllers/EscolasController.cs
+++ eEscola.API/Controllers/EscolasController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using eEscola.API.Models;
+using eEscola.API.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eEscola.API.Controllers
@@ -7,14 +8,56 @@ namespace eEscola.API.Controllers
     [ApiController]
     public class EscolasController : ControllerBase
     {
-        public EscolasController()
+        private readonly IEscolaRepository _escolaRepository;
+        public EscolasController(IEscolaRepository escolaRepository)
         {
+            _escolaRepository = escolaRepository;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok();
+            return Ok(await _escolaRepository.GetAll());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Escola escola)
+        {
+            if (await _escolaRepository.Add(escola))
+            {
+                return Ok("Cadastro realizado com sucesso!");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Escola escola)
+        {
+            var escolaDb = await _escolaRepository.GetById(id);
+            escolaDb.Nome = escola.Nome;
+            escolaDb.CNPJ = escola.CNPJ;
+
+            if (await _escolaRepository.Edit(escolaDb))
+            {
+                return Ok("Update realizado com sucesso!");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var escolaDb = await _escolaRepository.GetById(id);
+
+            if (escolaDb is not null)
+            {
+                await _escolaRepository.Delete(id);
+                return Ok("Excluído com sucesso!");
+            }
+
+            return BadRequest();
         }
     }
 }
